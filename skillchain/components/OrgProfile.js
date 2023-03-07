6753126e83add210c72b75b0407f739909515fe0
Michael <gadreelazazel@gmail.com>
2023-03-07 16:06:11 +0100

    Update

--- skillchain/components/OrgProfile.js
+++ skillchain/components/OrgProfile.js
@@ -220,13 +220,13 @@ export default function OrgProfile (){
                     {/*footer*/}
                     <div className='flex items-center justify-end p-6 bg-gray-800 border-t border-solid border-blueGray-200 rounded-b'>
                       <button
-                        className='text-white background-transparent font-bold uppercase text-sm px-6 py-3 bg-red-800 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                        className='text-white background-transparent font-bold text-sm px-6 py-3 bg-red-800 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                         type='button'
                         onClick={() => setShowModal(false)}>
                         Close
                       </button>
                       <button
-                        className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                        className='bg-emerald-500 text-white active:bg-emerald-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                         type='button'
                         onClick={handleAddEmployee}>
                         Add
